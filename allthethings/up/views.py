5d383b387cc9e299ff9685206a7958c9f61432c7
AnnaArchivist <1-AnnaArchivist@users.noreply.annas-software.org>
2023-01-30 00:00:00 +0300

    Automatically redirect to other mirror if we detect the current one is down

--- allthethings/up/views.py
+++ allthethings/up/views.py
@@ -1,14 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask_cors import CORS
 
 from allthethings.extensions import db
 from allthethings.initializers import redis
 
 
 up = Blueprint("up", __name__, template_folder="templates", url_prefix="/up")
+CORS(up)
 
 
 @up.get("/")
 def index():
+    # For testing, uncomment:
+    # if "testing_redirects" not in request.headers['Host']:
+    #     return "Simulate server down", 513
     return ""
 
 
